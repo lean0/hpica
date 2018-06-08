c0e37c48f06dc1aa410166099e350c56f9077455
lean0 <lkamanzintwali@efficom-lille.com>
2018-06-08 10:00:08 +0200

    v90

--- app/models/actu.rb
+++ app/models/actu.rb
@@ -1,9 +1,10 @@
 class Actu < ApplicationRecord
 
-  validates :title,uniqueness: {message: "existe déja !"},presence: {message: "est vide"}
-  validates :content, presence: {message: "est vide"}, uniqueness: {message: "existe déja !"}
-  validates :pwd, presence: {message: "est vide"}
+  validates :title,presence:{message:"est vide"},uniqueness:{message:"existe déja !"}
+  validates :content,presence:{message:"est vide"},uniqueness:{message:"existe déja !"}
+  validates :pwd,presence:{message:"est vide"}
+end
+
   # validates :image, presence: true
   # has_attached_file :image, styles: { :medium => '640x'}
   # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
-end
